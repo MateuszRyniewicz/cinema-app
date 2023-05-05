a3fb6f8649ffd0844695aafe608019f46d55b670
Mateusz <ryniuss@o2.pl>
2023-05-05 10:41:20 +0200

    add toogle function about text more

--- src/movies/components/MovieCard.js
+++ src/movies/components/MovieCard.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import './MovieCard.scss';
+import { text } from '@fortawesome/fontawesome-svg-core';
 //opis moze miec 100 znaków if >100 to wyśeitlają się 3 znaki po klik czyraj więcej opis sie rozrzezy.
 const MovieCard = ({
 	img,
@@ -12,7 +13,10 @@ const MovieCard = ({
 	description,
 	id,
 	linkToRead,
+	available,
 }) => {
+	const [isReadMore, setIsReadMore] = useState(false);
+
 	const navigate = useNavigate();
 
 	return (
@@ -28,7 +32,12 @@ const MovieCard = ({
 					<p>{time}</p>
 				</div>
 				<div className='card-details-box-text-main'>
-					{description}
+					{!isReadMore ? description.slice(0, 50) + '...' : description}
+					{description.length > 100 && (
+						<p onClick={() => setIsReadMore(!isReadMore)}>
+							{isReadMore ? '...show less' : '...show more'}
+						</p>
+					)}
 					<div className='card-details-box-text-main-link'>
 						<Link to={linkToRead} className='card-details-text-main-link'>
 							Więcej o filmie...
@@ -36,11 +45,15 @@ const MovieCard = ({
 					</div>
 				</div>
 				<div className='card-box-button'>
-					<button
-						className='card-button-go-to-booking-page'
-						onClick={() => navigate(`/movies/${id}`)}>
-						zobacz terminy
-					</button>
+					{available ? (
+						<button
+							className='card-button-go-to-booking-page'
+							onClick={() => navigate(`/movies/${id}`)}>
+							zobacz terminy
+						</button>
+					) : (
+						<p>brak terminów</p>
+					)}
 				</div>
 			</div>
 		</div>
@@ -48,5 +61,3 @@ const MovieCard = ({
 };
 
 export default MovieCard;
-//zrobić moviePage łanie ostylować. po klik w karte przenosi do cardPage. terminy dostępnych z listą miejsc.
-//projekt ma byc na git hube. czyste read me. ma to byc app do zazadzania kinem.
--- src/movies/pages/MoviesPage.js
+++ src/movies/pages/MoviesPage.js
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import LayoutWeb from '../../_shared/LayoutWeb';
 import MovieCard from '../components/MovieCard';
 
 import { movies } from '../../db/movies';
+import { movieTerms } from '../../db/movieTerms';
 
 import './MoviesPages.scss';
 
 const MoviesPage = () => {
+	const [test, setTest] = useState(false);
 	return (
 		<LayoutWeb>
 			<>
 				<div className='container-cards'>
-					{movies.map((movie) => (
-						<MovieCard
-							img={movie.img}
-							title={movie.title}
-							category={movie.category}
-							year={movie.year}
-							time={movie.time}
-							description={movie.description}
-							linkToRead={movie.linkToRead}
-							id={movie.id}
-						/>
-					))}
+					{movies.map((movie) => {
+						const terms = movieTerms.filter(
+							(term) => term.idMovie === movie.id
+						);
+						// {
+						// 	terms.map((term) => {
+						// 		const movie1 = movieTerms.filter(
+						// 			(movie1) => movie1.available == term.available
+						// 		);
+						// 		console.log(movie1);
+						// 	});
+						// }
+
+						return (
+							<MovieCard
+								img={movie.img}
+								title={movie.title}
+								category={movie.category}
+								year={movie.year}
+								time={movie.time}
+								description={movie.description}
+								linkToRead={movie.linkToRead}
+								id={movie.id}
+								available={false}
+							/>
+						);
+					})}
 				</div>
 			</>
 		</LayoutWeb>

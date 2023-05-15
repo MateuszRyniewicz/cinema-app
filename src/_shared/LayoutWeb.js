8b40e0f5a9ddf0d485c50e7473a010bdb5552f71
Mateusz <ryniuss@o2.pl>
2023-05-15 17:12:48 +0200

    CA:3 added nav to LayoutWeb

--- src/_shared/LayoutWeb.js
+++ src/_shared/LayoutWeb.js
@@ -4,27 +4,28 @@ import { Link } from 'react-router-dom';
 import './LayoutWeb.scss';
 
 const LayoutWeb = ({ children }) => {
-	
 	return (
 		<>
 			<header className='header'>
-				<ul className='menu'>
-					<li>
-						<Link className='menu-link' to='/'>
-							Home
-						</Link>
-					</li>
-					<li>
-						<Link className='menu-link' to='/movies'>
-							Filmy
-						</Link>
-					</li>
-					<li>
-						<Link className='menu-link' to='/booking'>
-							Rezerwacja
-						</Link>
-					</li>
-				</ul>
+				<nav className='nav'>
+					<ul className='menu'>
+						<li>
+							<Link className='menu-link' to='/'>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link className='menu-link' to='/movies'>
+								Filmy
+							</Link>
+						</li>
+						<li>
+							<Link className='menu-link' to='/booking'>
+								Rezerwacja
+							</Link>
+						</li>
+					</ul>
+				</nav>
 			</header>
 			<main className='main'>{children}</main>
 			<footer className='footer'>Movies App</footer>
